146d19644971066bacc46223c8327499ec0a99d1
Rubén Garrido <rgarridovaldepenas@uoc.edu>
2022-03-12 18:39:56 +0100

    Add Background + player and enemy to the main game scene

--- PEC1_Un-juego-de-aventuras/Assets/Scripts/GameManager.cs
+++ PEC1_Un-juego-de-aventuras/Assets/Scripts/GameManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Button newGameButton;
+    public Button loadGameButton;
+    public Button exitGameButton;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        AddListener(newGameButton);       
+        exitGameButton.onClick.AddListener(ExitGame);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void AddListener(Button button)
+    {
+        button.onClick.AddListener(() => { StartGame(); });
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Game"); 
+    }
+
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public static void EndGame()
+    {
+        SceneManager.LoadScene("EndGame");
     }
 }
